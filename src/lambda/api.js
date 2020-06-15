9a8ca165553da7161af659936c18c4d0b7d8c67e
essam <esamfarg91@gmail.com>
2020-06-15 17:27:07 +0200

    preparing mongodb connection

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -4,8 +4,23 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(
+    'mongodb+srv://essam:1234@test1-jvuqg.mongodb.net/test?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .catch((err) => {
+    console.log(err);
+  });
+mongoose.connection.once('open', () => console.log('database is connected'));
 
 const route = express.Router();
 app.use('/.netlify/functions/api', route);
