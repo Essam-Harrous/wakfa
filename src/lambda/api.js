0e0c9aa9be5edb6ab9f9b8fad944d6f266a6914b
essam <esamfarg91@gmail.com>
2020-06-15 16:54:14 +0200

    adding urlsencoded in lambda function

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.get('/h', (req, res) => {
+const route = express.Router();
+app.use('/.netlify/functions/api', route);
+
+route.get('/', (req, res) => {
   console.log('lambda is working');
-  res.send('lambda functions is working');
+  res.status(200).send('lambda functions is working');
 });
 
 module.exports.handler = serverless(app);
